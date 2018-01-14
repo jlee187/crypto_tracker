fe850ba1a95ac8c56f25f465f6c865fa18b6d9c2
jlee187 <jonathan.llee.8@gmail.com>
2018-01-14 01:45:36 -0500

    Add create crypto form + widget
    Users can now add a new crypto by typing in and submitting the form.
    This will persist to the DB and be registered to that respective Users
    
    Additionally, when a link is clicked into, a new chart widget is rendered

--- app/components/crypto-form.js
+++ app/components/crypto-form.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newCrypto: {
+    name: null,
+    quantity: null
+  },
+  actions: {
+    createCrypto () {
+      this.sendAction('createCrypto', this.get('newCrypto'));
+      this.set('newCrypto.name', null);
+      this.set('newCrypto.quantity', null);
+    }
+  }
 });
--- app/routes/cryptos.js
+++ app/routes/cryptos.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model () {
-    return this.get('store').findAll('crypto')
+    return this.get('store').findAll('crypto');
   },
+  actions: {
+    createCrypto(crypto) {
+      let newCrypto = this.get('store').createRecord('crypto', crypto);
+      newCrypto.save();
+    },
+  }
 });
