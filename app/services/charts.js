7b2873cd8dbeb59c04e0138cd8d9ed8f3b9a9cfe
jlee187 <jonathan.llee.8@gmail.com>
2018-01-18 11:20:10 -0500

    clean up css and functionalities.
    Still open is how to incorporate widget...
      1) this approach was to create a service to be called into sheet. Not sure i want to commit to this route yet.
    
    Moving onto making sure project is complete before proceeding further

---
+++ app/services/charts.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  // removeChart (model) {
+  //   return this.get('ajax').del(`/sign-out/${this.get('credentials.id')}`)
+  //   .finally(() => this.get('credentials').reset());
+  //   <script type="text/javascript" src = "https://widgets.cryptocompare.com/serve/v3/coin/chart?fsym=(model.name)&tsyms=USD,EUR,CNY,GBP">
+  //   </script>
+  // },
+});
